ae378ac4569e37c2097ba803845757ac0e341de6
AlecMcHaffie <AlecMcHaffie@outlook.com>
2015-07-22 14:24:47 +0100

    New Commit

---
+++ js/basics.js
@@ -0,0 +1,32 @@
+//DATATYPES
+//PRIMARY DATA TYPES
+//NUMBERS
+var number = 1;
+// isNan() to check if a number
+// nan = not a number
+
+//STRINGS
+var string = 'hello';
+var quote = '"Hello", Said the man';
+var escaped = '"Oh no you didn\'t" She said';
+var joined = 'wait, ' + 'what?';
+//Always encased in quotes
+
+//BOOLEAN
+
+//TRUE or FALSE
+
+//SPECIAL DATA TYPES
+//UNDEFINED
+//NULL
+
+
+//OPERATORS
+var addition = 1 + 1; //Addition = 2
+var subtraction = 1 - 1; // Subtraction = 0
+var multiplication = 5 * 2; // multiplication = 10
+var division = 5 / 2.5; // division = 2
+var remainder = 5 % 2; // remainder = 1
+
+var postfix = 5;
+var prefix = 5;
