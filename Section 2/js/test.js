84d57760d4273fffc0f6578661f7cb5b7c45723f
AlecMcHaffie <AlecMcHaffie@outlook.com>
2015-07-30 11:11:53 +0100

    Loops

--- Section 2/js/test.js
+++ Section 2/js/test.js
@@ -1,6 +1,8 @@
 (function() {
     'use strict';
 
+    //IF STATEMENTS//
+
     if (1 + 1 === 3) {
         console.log('The Expressions Is true');
     };
@@ -37,5 +39,52 @@
         console.log('No');
     };
 
-    
+    //SWITCH STATEMENTS//
+
+    var switcher = 'three';
+
+    switch (switcher) {
+        case 'one':
+            console.log('one');
+            break;
+        case 'two':
+            console.log('two');
+            break;
+        default:
+            console.log(switcher);
+
+    };
+
+    //LOOPS//
+
+    var anArray = ['Item1', 'Item2', 'Item3'];
+
+    for (var i = 0, x = anArray.length; i < x; i += 1) {
+        console.log(anArray[i]);
+    };
+
+    var anObject = {
+        prop1: 'property1',
+        prop2: 'property2',
+        prop3: 'property3',
+    };
+
+    for (var prop in anObject) {
+        if (anObject.hasOwnProperty(prop)) {
+            console.log(anObject[prop]);
+        }
+    }; //Slowest type of loop
+
+    var limit = 0;
+    while (limit < 5) {
+        limit ++;
+        console.log(limit);
+    };
+
+    var condition = true;
+    do {
+        console.log('once');
+        break;
+    } while (condition);
+
 }());
