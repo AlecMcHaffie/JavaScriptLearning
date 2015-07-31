820e99207331a9275764942dbbb5cc47f2e531f5
AlecMcHaffie <AlecMcHaffie@outlook.com>
2015-07-31 11:04:44 +0100

    Working with dates and times

---
+++ Section 4/js/dates.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+
+    var now = new Date();
+    console.log(now);
+    console.log(typeof Date());
+
+    var partyTime = new Date(946684799000);
+    console.log(partyTime);
+}());
