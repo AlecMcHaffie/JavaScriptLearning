a4432f4dcb7e5e175a583eab0a25e5389ed82ad8
AlecMcHaffie <AlecMcHaffie@outlook.com>
2015-07-31 11:29:37 +0100

    selecting html elements

---
+++ Section 5/js/elements.js
@@ -0,0 +1,7 @@
+(function() {
+    'use strict';
+
+    console.log(document.getElementById('mainHeading'));
+    console.log(document.getElementsByClassName('heading'));
+    console.log(document.getElementsByTagName('header'));
+}());
