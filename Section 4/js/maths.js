c635b4bf3b4a4ed1b93ce18aa61858e0afc8b234
AlecMcHaffie <AlecMcHaffie@outlook.com>
2015-07-31 11:16:51 +0100

    Working with the Math object

---
+++ Section 4/js/maths.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    //Constants
+    console.log(Math.PI);
+    console.log(Math.E);
+
+    //Rounding
+    console.log(Math.ceil(1.2)); //Rounds up
+    console.log(Math.floor(1.2)); //Round Down
+    console.log(Math.round(1.5)); //Rounds to the nearest integer
+
+    console.log(Math.max(1, 2, 6, 12)); //Returns largest number
+    console.log(Math.min(1,2,6,12)); // Returns Smallest
+    console.log(Math.sqrt(2)) //finds Square root
+
+    console.log(Math.random()); //Random number
+    console.log(Math.floor(Math.random() * 10)); //Random integer between 1 - 10
+}());
